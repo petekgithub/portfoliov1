8c6fe4bee8b139853291166ccaa0613f64fe6498
Petech <petekshamitbeyli@gmail.com>
2023-01-11 18:28:56 +0300

    timeline-portfolio-v2

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import timeline from "../data/timeline";
+import TimelineItem from "./TimelineItem";
 
 const Timeline = () => {
-  return <div>Timeline</div>;
+  return (
+    <div className="flex flex-col md:flex-row justify-center my-20">
+      <div className="w-full md:w-7/12">
+        {timeline.map((item) => (
+          <TimelineItem
+            year={item.year}
+            title={item.title}
+            duration={item.duration}
+            details={item.details}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Timeline;

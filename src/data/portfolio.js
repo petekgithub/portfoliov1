983ba9a41eb3332178330c0bad055f28a0946b8f
Petech <petekshamitbeyli@gmail.com>
2023-01-14 00:54:07 +0300

    portfolio-assets-update

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,33 +1,33 @@
 export default [
   {
     title: "Qute Generator",
-    imgUrl: "src/assets/quote.png",
+    imgUrl: "src/assets/p1.png",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://quote-generator-lilac-eta.vercel.app/",
   },
   {
     title: "Cryptocurrency Tracker",
-    imgUrl: "src/assets/quote.png",
+    imgUrl: "src/assets/p3.png",
     stack: ["ReactJs", "JavaScript"],
     link: "https://cryptocurrency-tracker-puce.vercel.app/",
   },
   {
     title: "Find Heroes",
-    imgUrl: "src/assets/quote.png",
+    imgUrl: "src/assets/p6.png",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://find-heroes.vercel.app/",
   },
   {
-    title: "Uifry LandingPage",
-    imgUrl: "src/assets/quote.png",
-    stack: ["NextJS", "SCSS", "TypeScript"],
-    link: "",
-  },
-  {
     title: "FyloDark LandingPage",
-    imgUrl: "src/assets/quote.png",
+    imgUrl: "src/assets/p7.png",
     stack: ["NextJS", "CSS", "TypeScript"],
     link: "https://fylo-dark-landing-page-five.vercel.app/",
+  },
+  {
+    title: "Uifry LandingPage",
+    imgUrl: "src/assets/p5.png",
+    stack: ["NextJS", "SCSS", "TypeScript"],
+    link: "",
   },
   {
     title: "Guess Weather",

a424a93099a68348f155da097b6155d1e3854122
Petech <petekshamitbeyli@gmail.com>
2023-01-17 00:14:58 +0300

    icons-v1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="py-5 text-center">
-      {/* Social Icons */}
       <p className="text-sm mt-2 opacity-50">
         &copy; {new Date().getFullYear()} Petek Savaş Hamibeyli. All rights
         reserved.
